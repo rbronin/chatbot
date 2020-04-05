0814e0a8fac28bc63a8e4691a5c574d1ce52a4bc
Ravi Bharti <47503503+ravics1721@users.noreply.github.com>
2020-04-06 04:41:55 +0530

    new setup commit

--- app.js
+++ app.js
@@ -2,55 +2,55 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const apiai = require('api.ai');
-const port = 3000 || process.env.PORT;
+const apiai = require("api.ai");
+const port = 3000 || process.env.PORT;  // defining the port
 
 
 const app = express();
 
-  app.use(express.static('public'));
-  app.set('view engine', 'ejs');
-  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static("public")); //serving static files directory
+  app.set('view engine', 'ejs'); // template engine setup 
+  app.set('views', (__dirname, 'views')); // set the view directory
+  app.use(bodyParser.urlencoded({ extended: true })); // body parser setup 
 
+  // setting up the api credentials
   const ai = new apiai({
     token: process.env.CLIENT_TOKEN,
-    session: process.env.API_KEY
+    session: process.env.SESSION_ID
   });
 
-const newMessage = [];
 
+  let userInput = "";
+  let data;
 
+///// making  get request on home route
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render('index', {userMessage: userInput, botReply: data});  
 });
 
-app.post("/", (req, res) => {
-    const message = req.body.message;
 
-      
+///// post request on home route
+app.post("/", (req, res) => {
+    
+    let message = req.body.msg;   // getting data from user input
+    userInput = message;
+
+    /// requesting the user message on api.ai
     ai.text(message, (error, response) => {
         if(error) {
             console.log(error);
             
         } else {
-            const data = response.result.fulfillment.speech;
+            data = response.result.fulfillment.speech; /// getting the response result
            console.log(data);
-           
-            newMessage.push(data);
-            res.redirect("message");
+           res.redirect("/");
         }
     });
+   
     
 });
 
-
-app.get("/message", (req, res) => {
-
-    res.send("<h1>" + newMessage + "</h1>");
-});
-
-
-
+/// server setup
 app.listen(port, (req, res) => {
     console.log(`server is running on ${port}`);
     
